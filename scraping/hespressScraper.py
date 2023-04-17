4c99883c9465b0900ff2164fe285c8135c083800
Mustapha EZZALI <mustapha_ezzali@um5.ac.ma>
2023-04-17 18:37:20 +0000

    scraping dates urls titles

--- scraping/hespressScraper.py
+++ scraping/hespressScraper.py
@@ -4,12 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import csv
 
 driver = webdriver.Chrome('.\chromedriver.exe')
 
 #driver.get('https://www.hespress.com/')
 
-categories = ["https://www.hespress.com/politique",
+categories_links = ["https://www.hespress.com/politique",
               "https://www.hespress.com/regions",
               "https://www.hespress.com/societe",
               "https://www.hespress.com/economie",
@@ -19,6 +20,7 @@ categories = ["https://www.hespress.com/politique",
               "https://www.hespress.com/sport",
               "https://www.hespress.com/tamazight",
             ]
+categories = ["Politique",'regions','societe','economie','faits-divers','medias','art-et-culture','sport','tamazight']
 
 driver.get(categories[0])
 driver.implicitly_wait(10)
@@ -33,12 +35,28 @@ for i in range(10):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(1)
 links =  WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH,"//a[contains(@class,'stretched-link')]")))
-
+datetime = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@class,'card-details')]//div"))) 
+title = WebDriverWait(driver, 5).until(EC.presence_of_all_elements_located((By.XPATH,"//div[contains(@class,'card-details')]//h3")))
+titles = []
 urls = []
+dates = []
+
 for link in links:
     url = link.get_attribute('href')
     urls.append(url)
+for date in datetime:
+    date = date.text
+    dates.append(date)
+for t in title:
+    t = t.text
+    titles.append(t)
+
+with open('hespress.csv', 'w',encoding='utf-8', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Date", "URL"])
+    for i in range(len(titles)):
+        writer.writerow([titles[i], dates[i], urls[i]])
 
 # Print the extracted links
-print(urls,len(urls))
+print(dates,len(dates),len(urls))
 
